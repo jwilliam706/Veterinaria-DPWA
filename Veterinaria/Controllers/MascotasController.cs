065bfd4bec370855a71fe336518a5d252e92db48
William Martinez <williamr1994@gmail.com>
2023-06-10 13:33:30 -0600

    agregando db AWS y configuracion inicial de CRUDs

---
+++ Veterinaria/Controllers/MascotasController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Veterinaria.Controllers
+{
+    public class MascotasController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Detalle(int id)
+        {
+            ViewBag.id = id;
+            return View();
+        }
+    }
+}
